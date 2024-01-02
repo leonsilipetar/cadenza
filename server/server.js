84461882481488bacc570f385be31a45bc657343
leonsilipetar <101987418+leonsilipetar@users.noreply.github.com>
2024-01-02 20:59:56 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+app.use(cors({credentials: true, origin: "https://mai-cadenza.onrender.com"}));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api', router);
