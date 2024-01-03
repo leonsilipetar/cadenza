227e95c7e0eaa9447ce1f3367f2a4ff26e102241
leon <leon.silipetar@gmail.com>
2024-01-03 16:55:02 +0100

    update

--- server/server.js
+++ server/server.js
@@ -14,10 +14,7 @@ app.use(
   cors({
     credentials: true,
     origin: function (origin, callback) {
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'https://mai-cadenza.onrender.com',
-      ];
+      const allowedOrigins = ['http://localhost:3000', 'https://mai-cadenza.onrender.com'];
 
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
@@ -27,6 +24,10 @@ app.use(
     },
   })
 );
+
+// Handle preflight requests
+app.options('*', cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api', router);
@@ -43,8 +44,9 @@ const PORT = process.env.PORT || 5000;
 mongoose.set('strictQuery', false);
 mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.r3ypmfa.mongodb.net/`)
   .then(() => {
-    app.listen(PORT);
-    console.log("Database is connected");
-    console.log(`Listening on port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log("Database is connected");
+      console.log(`Listening on port ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
