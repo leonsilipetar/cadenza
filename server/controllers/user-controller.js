0f4087c2bd9e77f3848ae3503cce5c3eec35864e
leon <leon.silipetar@gmail.com>
2024-01-03 20:31:42 +0100

    update

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -152,21 +152,29 @@ const signup = async (req, res, next) => {
     };
     
   
-  const verifyToken = (req, res, next) => {
-    const cookies = req.headers.cookie;
-    const token = cookies.split("=")[1];
-    if (!token) {
-      return res.status(404).json({ message: "No token found" });
-    }
-    jwt.verify(String(token), process.env.JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.status(400).json({ message: "Invalid Token" });
+    const verifyToken = (req, res, next) => {
+      const cookies = req.headers.cookie;
+    
+      if (!cookies) {
+        return res.status(404).json({ message: "No cookies found" });
       }
-      console.log(user.id);
-      req.id = user.id;
-      next();
-    });
-  };
+    
+      const tokenString = cookies.split("=")[1];
+    
+      if (!tokenString) {
+        return res.status(404).json({ message: "No token found" });
+      }
+    
+      jwt.verify(tokenString, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+          return res.status(400).json({ message: "Invalid Token" });
+        }
+    
+        console.log(user.id);
+        req.id = user.id;
+        next();
+      });
+    };
   
   async function getUser(req, res, next) {
     const userId = req.id;
