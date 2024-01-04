c6357b79301dc242e4a43ca7abc7f882a196e07c
leon <leon.silipetar@gmail.com>
2024-01-04 10:40:30 +0100

    update-render

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -226,22 +226,23 @@ const refreshToken = (req, res, next) => {
 };
 
 const logout = (req, res, next) => {
-    const cookies = req.headers.cookie;
-    const prevToken = cookies.split("=")[1];
-    if(!prevToken) {
-        return res.status(400).json({message: "Couldn't find token"});
-    }
-    jwt.verify(String(prevToken), process.env.JWT_SECRET, (err, user) => {
-        if(err) {
-            console.log(err);
-            return res.status(403).json({message: "Autentication faild"})
-        };
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
 
-        res.clearCookie(`${user.id}`);
-        req.cookies[`${user.id}`] = "";
-        return res.status(200).json({message: "Successfully Logged Out"})
-    })
-}
+  if (!prevToken) {
+      return res.status(400).json({ message: "Couldn't find token" });
+  }
+
+  jwt.verify(String(prevToken), process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+          console.log(err);
+          return res.status(403).json({ message: "Authentication failed" });
+      }
+
+      res.clearCookie(`${user.id}`);
+      return res.status(200).json({ message: "Successfully Logged Out" });
+  });
+};
 const getKorisnici = async (req, res, next) => {
   try {
     // Select only specific fields (ime, prezime, program) from the database
