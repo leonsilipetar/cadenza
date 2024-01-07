5849342b814f1d9c0a034327b97f86eb19e3c4d1
leon <leon.silipetar@gmail.com>
2024-01-07 17:38:49 +0100

    update

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -140,7 +140,7 @@ const signup = async (req, res, next) => {
         res.cookie(String(existingUser._id), token, {
           path: '/',
           httpOnly: true,
-          sameSite: 'lax', //on localhost is lax, on render is none
+          sameSite: 'none', //on localhost is lax, on render is none
           secure: process.env.NODE_ENV === 'production',/* on localhost is false*/
         });
     
