899d3af0ec96639d48665daf7729ec88bb2d4da1
leon <leon.silipetar@gmail.com>
2024-01-11 16:29:51 +0100

    update

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -265,6 +265,7 @@ const logout = (req, res, next) => {
     console.log('Clearing cookie for user:', user.id);
     res.clearCookie(String(user.id), {
       path: '/',  // Specify the same path used when setting the cookie
+      domain: "mai-cadenza.onrender.com",
       secure: process.env.NODE_ENV === 'production',  // Set to true if using HTTPS
     });
 
