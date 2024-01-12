24fa867daae8332212f67a95ae01505197c27239
leon <leon.silipetar@gmail.com>
2024-01-12 14:05:17 +0100

    update

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -149,7 +149,7 @@ const sendPasswordEmail = async (email, password) => {
         res.cookie(String(existingUser._id), token, {
           path: '/',
           httpOnly: true,
-          sameSite: 'lax', //localhost -- lax
+          sameSite: 'none', //localhost -- lax
           secure: process.env.NODE_ENV === 'production',
         });
     
