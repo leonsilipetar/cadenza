c6739aa30052d820ea4950c53d3c4a37bf2c2326
leon <leon.silipetar@gmail.com>
2024-01-13 20:14:07 +0100

    update

--- server/server.js
+++ server/server.js
@@ -23,7 +23,23 @@ app.use(
     },
   })
 );
+/*
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
 
+app.get("/*", function(req, res){
+
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function(err) {
+      if(err){
+        res.status(500),send(err);
+      }
+    }
+  )
+})
+*/
 // Handle preflight requests
 app.options('*', cors());
 
