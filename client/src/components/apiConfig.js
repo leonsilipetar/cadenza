985703c3b4d93f0b984302e1d4d3eb2445b08133
Leon <leon.silipetar@]mail.com>
2024-09-24 18:36:27 +0200

    update

--- client/src/components/apiConfig.js
+++ client/src/components/apiConfig.js
@@ -1,7 +1,7 @@
 const ApiConfig = {
   // Set the base URL conditionally
   baseUrl: process.env.NODE_ENV === 'production'
-    ? 'https://maiweb-server.onrender.com'
+    ? 'https://musicartincubator-cadenza.onrender.com'
     : 'http://localhost:5000',
 };
 
