bbb952e7cd5b423195d057ea6fad2f6292b06111
Leon <leon.silipetar@]mail.com>
2024-11-21 09:52:56 +0100

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
 import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
+
 import { store } from './store';
+
+
 
 const root = document.getElementById('root');
 
+
+
 // Replace ReactDOM.render with createRoot().render
+
 const rootElement = ReactDOM.createRoot(root);
+
 rootElement.render(
-  <BrowserRouter basename="/">
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  </BrowserRouter>
+
+  <Provider store={store}>
+
+    <BrowserRouter>
+
+      <App />
+
+    </BrowserRouter>
+
+  </Provider>
+
 );
+
+
 
--- server/server.js
+++ server/server.js
@@ -1,59 +1,56 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = require('./routes/user-routes.js');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const ServerConfig = require('./serverConfig');
-require('dotenv').config();
-const recipeRoutes = require('./routes/recipe-routes.js');
-
-const app = express();
-
-app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      const allowedOrigins = ['http://localhost:3000', 'https://cadenza.com.hr'];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  })
-);
-
-
-// Handle preflight requests
-app.options('*', cors());
-
-app.use(cookieParser());
-app.use(express.json());
-app.use('/api', router);
-app.use('/api/recepti', recipeRoutes);
-
-// Serve static files from the React app
-const buildPath = path.join(__dirname, '../client/build');
-app.use(express.static(buildPath));
-
-// The catch-all handler: for any request that doesn't match the API routes, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.r3ypmfa.mongodb.net/`)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("Database is connected");
-      console.log(`Listening on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/user-routes.js');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const ServerConfig = require('./serverConfig');
+require('dotenv').config();
+const recipeRoutes = require('./routes/recipe-routes.js');
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:3000', 'https://cadenza.com.hr'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
+
+
+// Handle preflight requests
+app.options('*', cors());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api', router);
+app.use('/api/recepti', recipeRoutes);
+
+// Serve static files from the React app
+const buildPath = path.join(__dirname, '../client/build');
+app.use(express.static(buildPath));
+
+// The catch-all handler: for any request that doesn't match the API routes, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.r3ypmfa.mongodb.net/`)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database is connected");
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
