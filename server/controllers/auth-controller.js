5affce0b265d3ce200ebb742f075878fe2bad1a0
Leon <leon.silipetar@]mail.com>
2025-01-11 18:51:25 +0100

    v1.0.9

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -47,7 +47,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'None',
+      sameSite: 'lax',
       path: '/',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
