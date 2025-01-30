173e50cae37e85d3b9cee05b11361ecb7f3f435f
Leon <leon.silipetar@]mail.com>
2025-01-30 20:30:18 +0100

    v1.0.0

--- client/src/components/apiConfig.js
+++ client/src/components/apiConfig.js
@@ -1,12 +1,13 @@
 const ApiConfig = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? 'https://musicartincubator-cadenza.onrender.com'
-    : 'http://localhost:5000',
+  baseUrl:
+    typeof window !== "undefined" && window.location.hostname === "localhost"
+      ? "http://localhost:5000"
+      : "https://musicartincubator-cadenza.onrender.com",
   cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    path: '/'
-  }
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+  },
 };
 
 export default ApiConfig;
